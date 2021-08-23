5d1fc28df4c6f69c30d5ac56788464ed808fc4a6
simoncerman <mineropka34@gmail.com>
2021-08-23 20:34:46 +0200

    Upgradeeee

--- accessable/script.js
+++ accessable/script.js
@@ -3,7 +3,6 @@ function getData(){
     fetch('/data')
     .then(response=>response.json())
     .then(data =>{
-        console.log(data);
         for (let y = 0; y < data.length; y++) {
             const element = data[y];
             let box = document.createElement("div");
@@ -11,7 +10,27 @@ function getData(){
             let h3 = document.createElement("h3");
             h3.innerText = element.activity;
             box.appendChild(h3);
-            document.getElementById('content').appendChild(box);          }
+            document.getElementById('content').appendChild(box);
+        }
 
     })
+}
+function choseOne(){
+    let isChosed = false;
+    let boxes = document.getElementsByClassName('box');
+    for (let i = 0; i < boxes.length; i++) {
+        const element = boxes[i];
+        if(element.style.backgroundColor == "red"){
+            isChosed = true;
+        }
+    }
+    if(isChosed==false){
+        let boxList = document.getElementsByClassName("box");
+        let randomNumber = Math.floor(Math.random()*boxList.length);
+        boxList[randomNumber].style.backgroundColor = "red";
+    }
+}
+function reloadData(){
+    document.getElementById('content').innerHTML = '';
+    getData();
 }
